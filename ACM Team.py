104a848020ba199c490571d9a7cba49a68f7cd7e
ArzuEl1yeva <arzueliyeva0212@gmail.com>
2025-07-04 18:36:15 +0400

    Update ACM Team.py

--- ACM Team.py
+++ ACM Team.py
@@ -1,13 +1,14 @@
-def acm_team(topic):
+def acm_team(topic: list[str]) -> list[int]:
+    n = len(topic)
     max_topics = 0
     team_count = 0
 
-    n = len(topic)
     for i in range(n):
         for j in range(i + 1, n):
-            combined = int(topic[i], 2) | int(topic[j], 2)
-            topics_known = bin(combined).count('1')
-
+            topics_known = 0
+            for k in range(len(topic[i])):
+                if topic[i][k] == '1' or topic[j][k] == '1':
+                    topics_known += 1
             if topics_known > max_topics:
                 max_topics = topics_known
                 team_count = 1
@@ -15,4 +16,3 @@ def acm_team(topic):
                 team_count += 1
 
     return [max_topics, team_count]
-print(acm_team(["000", "000", "000"]))
